57cf8bc66edfe6a1426c2bfcaf962df8d6535940
John <john@example.com>
2023-11-30 10:13:03 +0530

    env usage

--- src/services/custom/api.js
+++ src/services/custom/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 
-export let baseURL = "http://localhost:4445"
-if(process.env.REACT_APP_ENV === 'server'){
-  baseURL = "http://13.233.191.14:4445";
-}
+export const baseURL = process.env.REACT_API_BASE_URL;
 
 export const getToken = () => {
   let authInfo = localStorage.getItem("ta-auth")
